fffffa7f7ae522b8d18aa00f56928f8b9a3b65cb
scx1332 <sieciech.czajka@golem.network>
2024-07-29 12:27:22 +0200

    experimenting with loopback

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,158 @@
 mod utils;
+mod loopback;
 
+use core::str;
 use std::env;
-use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use dns_parser::QueryType;
-use pcap::{Capture, Device};
-use smoltcp::{
-    phy::DeviceCapabilities,
-    wire::{EthernetAddress, IpAddress, IpCidr},
-};
-use std::os::fd::AsRawFd;
-use smoltcp::phy::{Medium, TunTapInterface};
-use tokio::io::{copy, split, AsyncReadExt, AsyncWriteExt};
-use tokio_smoltcp::{device::AsyncDevice, smoltcp::iface, Net, NetConfig};
+use log::{debug, error, info};
+
+use smoltcp::iface::{Config, Interface, SocketSet};
+use smoltcp::phy::{Device, Medium, PcapMode, PcapWriter};
+use smoltcp::socket::tcp;
+use smoltcp::time::{Duration, Instant};
+use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
+use crate::loopback::Loopback;
+
+
+mod mock {
+    use smoltcp::time::{Duration, Instant};
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
+
+    // should be AtomicU64 but that's unstable
+    #[derive(Debug, Clone)]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+    pub struct Clock(Arc<AtomicUsize>);
+
+    impl Clock {
+        pub fn new() -> Clock {
+            Clock(Arc::new(AtomicUsize::new(0)))
+        }
+
+        pub fn advance(&self, duration: Duration) {
+            self.0
+                .fetch_add(duration.total_millis() as usize, Ordering::SeqCst);
+        }
+
+        pub fn elapsed(&self) -> Instant {
+            Instant::from_millis(self.0.load(Ordering::SeqCst) as i64)
+        }
+    }
+}
+
+fn create_tcp_socket() -> tcp::Socket<'static> {
+    let rx_buffer = vec![0; 1024];
+    let tx_buffer = vec![0; 1024];
+    let tcp_rx_buffer = tcp::SocketBuffer::new(rx_buffer);
+    let tcp_tx_buffer = tcp::SocketBuffer::new(tx_buffer);
+    tcp::Socket::new(tcp_rx_buffer, tcp_tx_buffer)
+}
+
+
+async fn async_main(opt: Opt) -> anyhow::Result<()> {
+    let clock = mock::Clock::new();
+    let device = Loopback::new(Medium::Ethernet);
+
+
+    let mut device = {
+        //let clock = clock.clone();
+        //utils::setup_logging_with_clock("", move || clock.elapsed());
+
+        let (mut opts, mut free) = utils::create_options();
+        utils::add_middleware_options(&mut opts, &mut free);
+
+        let mut matches = utils::parse_options(&opts, free);
+        utils::parse_middleware_options(&mut matches, device, /*loopback=*/ true)
+    };
+
+    // Create interface
+    let mut config = match device.capabilities().medium {
+        Medium::Ethernet => {
+            println!("Ethernet");
+            Config::new(EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x01]).into())
+        }
+        Medium::Ip => Config::new(smoltcp::wire::HardwareAddress::Ip),
+    };
+
+    let mut iface = Interface::new(config, &mut device, Instant::now());
+    iface.update_ip_addrs(|ip_addrs| {
+        ip_addrs
+            .push(IpCidr::new(IpAddress::v4(127, 0, 0, 1), 8))
+            .unwrap();
+    });
+
+    // Create sockets
+    let server_socket = create_tcp_socket();
+
+    let client_socket = create_tcp_socket();
+
+    let mut sockets: [_; 2] = Default::default();
+    let mut sockets = SocketSet::new(&mut sockets[..]);
+    let server_handle = sockets.add(server_socket);
+    let client_handle = sockets.add(client_socket);
+
+    let mut did_listen = false;
+    let mut did_connect = false;
+    let mut done = false;
+    while !done && clock.elapsed() < Instant::from_millis(10_000) {
+        iface.poll(clock.elapsed(), &mut device, &mut sockets);
+
+        let mut socket = sockets.get_mut::<tcp::Socket>(server_handle);
+        if !socket.is_active() && !socket.is_listening() {
+            if !did_listen {
+                debug!("listening");
+                socket.listen(1234).unwrap();
+                did_listen = true;
+            }
+        }
+
+        if socket.can_recv() {
+            debug!(
+                "got {:?}",
+                socket.recv(|buffer| { (buffer.len(), str::from_utf8(buffer).unwrap()) })
+            );
+            socket.close();
+            done = true;
+        }
+
+        let mut socket = sockets.get_mut::<tcp::Socket>(client_handle);
+        let cx = iface.context();
+        if !socket.is_open() {
+            if !did_connect {
+                debug!("connecting");
+                socket
+                    .connect(cx, (IpAddress::v4(127, 0, 0, 1), 1234), 65000)
+                    .unwrap();
+                did_connect = true;
+            }
+        }
+
+        if socket.can_send() {
+            debug!("sending");
+            socket.send_slice(b"0123456789abcdef").unwrap();
+            socket.close();
+        }
+
+        match iface.poll_delay(clock.elapsed(), &sockets) {
+            Some(Duration::ZERO) => debug!("resuming"),
+            Some(delay) => {
+                debug!("sleeping for {} ms", delay);
+                clock.advance(delay)
+            }
+            None => clock.advance(Duration::from_millis(1)),
+        }
+    }
+
+    if done {
+        log::info!("done")
+    } else {
+        log::error!("this is taking too long, bailing out")
+    }
+    Ok(())
+}
 
 #[derive(Debug, Parser)]
 struct Opt {
-    device: String,
     #[clap(short, long, default_value = "00:01:02:03:04:05")]
     ethernet_addr: String,
     #[clap(short, long, default_value = "192.168.69.1/24")]
@@ -25,131 +161,9 @@ struct Opt {
     gateway: String,
 }
 
-fn get_by_device(device: Device) -> Result<impl AsyncDevice> {
-    use std::io;
-    use tokio_smoltcp::device::AsyncCapture;
-
-    let cap = Capture::from_device(device.clone())
-        .context("Failed to capture device")?
-        .promisc(true)
-        .immediate_mode(true)
-        .timeout(5)
-        .open()
-        .context("Failed to open device")?;
-
-    fn map_err(e: pcap::Error) -> io::Error {
-        match e {
-            pcap::Error::IoError(e) => e.into(),
-            pcap::Error::TimeoutExpired => io::ErrorKind::WouldBlock.into(),
-            other => io::Error::new(io::ErrorKind::Other, other),
-        }
-    }
-    let mut caps = DeviceCapabilities::default();
-    match caps.medium {
-        smoltcp::phy::Medium::Ethernet => {
-            println!("Detected medium: Ethernet");
-        }
-        _ => return Err(anyhow!("Unsupported medium")),
-    }
-    caps.max_burst_size = Some(100);
-    caps.max_transmission_unit = 1500;
-
-
-    let device = TunTapInterface::new("tap0", Medium::Ethernet).context("Failed to create tun/tap interface")?;
-    //let fd = device.as_raw_fd();
-    Ok(AsyncCapture::new(
-        device,
-        |d| {
-            let r = d.receive().map_err(map_err).map(|p| p.to_vec());
-            // eprintln!("recv {:?}", r);
-            r
-        },
-        |d, pkt| {
-            let r = d.sendpacket(pkt).map_err(map_err);
-            // eprintln!("send {:?}", r);
-            r
-        },
-        caps,
-    )
-        .context("Failed to create async capture")?)
-}
-
-
-async fn async_main(opt: Opt) -> Result<()> {
-    //print all devices
-    for device in Device::list()? {
-        println!("Device: {:?}", device);
-    }
-    let device = Device::list()?
-        .into_iter()
-        .find(|d| d.name == opt.device)
-        .ok_or(anyhow!("Device not found"))?;
-    let ethernet_addr: EthernetAddress = opt.ethernet_addr.parse().unwrap();
-    let ip_addr: IpCidr = opt.ip_addr.parse().unwrap();
-    let gateway: IpAddress = opt.gateway.parse().unwrap();
-
-    let device = get_by_device(device)?;
-    let mut interface_config = iface::Config::new(ethernet_addr.into());
-    interface_config.random_seed = rand::random();
-    let net = Net::new(
-        device,
-        NetConfig::new(interface_config, ip_addr, vec![gateway]),
-    );
-
-    let udp = net.udp_bind("0.0.0.0:0".parse()?).await?;
-    println!("udp local_addr {:?}", udp.local_addr());
-    let mut query_builder = dns_parser::Builder::new_query(1, true);
-    query_builder.add_question(
-        "www.baidu.com",
-        false,
-        QueryType::A,
-        dns_parser::QueryClass::IN,
-    );
-    let query = query_builder.build().unwrap();
-    udp.send_to(&query, "8.8.8.8:53".parse()?).await?;
-    let mut answer = [0u8; 1024];
-    let (size, _) = udp.recv_from(&mut answer).await?;
-    let packet = dns_parser::Packet::parse(&answer[..size])?;
-    println!("dns answer packet: {:#?}", packet);
-    let dst_ip = packet
-        .answers
-        .iter()
-        .filter_map(|a| match a.data {
-            dns_parser::RData::A(dns_parser::rdata::A(ip)) => Some(ip),
-            _ => None,
-        })
-        .next()
-        .expect("No A record in response");
-
-    println!("Connecting www.baidu.com");
-    let mut tcp = net.tcp_connect((dst_ip, 80).into()).await?;
-    println!("Connected");
-
-    tcp.write_all(b"GET / HTTP/1.0\r\nHost: www.baidu.com\r\n\r\n")
-        .await?;
-    println!("Sent");
-
-    let mut string = String::new();
-    tcp.read_to_string(&mut string).await?;
-    println!("read {}", string);
-
-    let mut listener = net.tcp_bind("0.0.0.0:12345".parse()?).await?;
-    loop {
-        let (tcp, addr) = listener.accept().await?;
-        println!("Accept from {:?}", addr);
-        tokio::spawn(async move {
-            let (mut tx, mut rx) = split(tcp);
-            copy(&mut tx, &mut rx).await.unwrap();
-        });
-    }
-
-    // Ok(())
-}
-
-
 #[tokio::main]
-async fn main() -> Result<()> {
-    env::set_var("RUST_LOG", env::var("RUST_LOG").unwrap_or("info".to_string()));
+async fn main() -> anyhow::Result<()> {
+    env::set_var("RUST_LOG", env::var("RUST_LOG").unwrap_or("trace".to_string()));
     env_logger::init();
 
     let opt = Opt::parse();
